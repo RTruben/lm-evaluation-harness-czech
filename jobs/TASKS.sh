b01355d26a21b30f7ed00200aa032eeb4682ef28
Martin Fajčík <martin.fajcik@vut.cz>
2024-10-04 16:42:04 +0200

    disabled tasks that didn't passed the contamination check

--- jobs/TASKS.sh
+++ jobs/TASKS.sh
@@ -4,13 +4,13 @@
 export TASKS=(
     "benczechmark_agree"                   #0
     "benczechmark_belebele"                #1
-    "benczechmark_czechnews"               #2
-    "benczechmark_snli" #3
+#    "benczechmark_czechnews"              #2 # REMOVED DUE EXCESSIVE CONTAMINATION
+    "benczechmark_snli"                    #3
     "benczechmark_subjectivity"            #4
     "benczechmark_propaganda_argumentace"  #5
     "benczechmark_propaganda_fabulace"     #6
     "benczechmark_propaganda_nazor"        #7
-    "benczechmark_propaganda_strach" #8
+    "benczechmark_propaganda_strach"       #8
     "benczechmark_propaganda_zamereni"     #9
     "benczechmark_propaganda_demonizace"   #10
     "benczechmark_propaganda_lokace"       #11
@@ -23,33 +23,33 @@ export TASKS=(
     "benczechmark_sentiment_mall"          #18
     "benczechmark_sentiment_fb"            #19
     "benczechmark_sentiment_csfd"          #20
-    "benczechmark_summarization" #21
+#    "benczechmark_summarization"          #21 # REMOVED DUE EXCESSIVE CONTAMINATION
     "benczechmark_grammarerrorcorrection"  #22
-    "benczechmark_cs_naturalquestions" #23
-    "benczechmark_cs_sqad32"           #24
-    "benczechmark_cs_triviaQA"         #25
+    "benczechmark_cs_naturalquestions"     #23
+    "benczechmark_cs_sqad32"               #24
+    "benczechmark_cs_triviaQA"             #25
     "benczechmark_csfever_nli"             #26
     "benczechmark_ctkfacts_nli"            #27
-    "benczechmark_cs_ner" #28
+    "benczechmark_cs_ner"                  #28
     "benczechmark_hellaswag"               #29
-    "benczechmark_klokan_qa"              #30
-    "benczechmark_cs_court_decisions_ner" #31
+    "benczechmark_klokan_qa"               #30
+    "benczechmark_cs_court_decisions_ner"  #31
     "benczechmark_umimeto_qa"              #32
     "benczechmark_cermat_mc"               #33
-    "benczechmark_cermat_qa" #34
+    "benczechmark_cermat_qa"               #34
     "benczechmark_history_ir"              #35
     "benczechmark_histcorpus"              #36
     "benczechmark_essay"                   #37
     "benczechmark_fiction"                 #38
-    "benczechmark_capek"                   #39
+#    "benczechmark_capek"                  #39 # REMOVED DUE EXCESSIVE CONTAMINATION
     "benczechmark_correspondence"          #40
     "benczechmark_havlicek"                #41
-    "benczechmark_speeches"                #42
+#    "benczechmark_speeches"               #42 # REMOVED DUE EXCESSIVE CONTAMINATION
     "benczechmark_spoken"                  #43
     "benczechmark_dialect"                 #44
 )
 
-# Define tasks that require summing of logprobs
+# Define language modeling tasks that require summing of logprobs
 SUM_LOGPROBS=(
   "benczechmark_histcorpus"
   "benczechmark_essay"
