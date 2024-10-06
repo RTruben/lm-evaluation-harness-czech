b66ce77addfb3bdc7a7d655796f3deca488bdcb8
Miroslav Hrabal <mhn@email.cz>
2024-10-06 22:49:04 +0200

    fix: call qsub instead of squeue

--- jobs/run_benchmark_metacentrum.sh
+++ jobs/run_benchmark_metacentrum.sh
@@ -4,4 +4,4 @@ source ./jobs/TASKS.sh
 
 # S MODELS - EACH JOB USES SINGLE GPU
 # sbatch array job, number of tasks is the length of the array TASKS
-sbatch -J 0-$((${#TASKS[@]} - 1)) jobs/scripts/submit/models_S/eval_cuni_mh.metacentrum.sh
+qsub -J 0-$((${#TASKS[@]} - 1)) jobs/scripts/submit/models_S/eval_cuni_mh.metacentrum.sh
