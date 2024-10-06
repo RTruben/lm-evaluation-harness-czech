0921a74c00a53b2c0560fcb0726d4cf08ae2a8a7
Miroslav Hrabal <mhn@email.cz>
2024-10-06 23:12:14 +0200

    fix: enter the script dir on start

--- jobs/scripts/submit/models_S/eval_cuni_mh.metacentrum.sh
+++ jobs/scripts/submit/models_S/eval_cuni_mh.metacentrum.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
 #PBS -N llm_harness_cuni_mh
 #PBS -q gpu
-#PBS -l select=1:ncpus=1:mem=32gb:scratch_local=48gb:ngpus=1:gpu_cap=compute_70:gpu_mem=35gb:cl_zia=True
+#PBS -l select=1:ncpus=1:mem=32gb:scratch_local=48gb:ngpus=1:gpu_cap=compute_70:gpu_mem=35gb
 #PBS -l walltime=18:00:00
 #PBS -m bae
 
 NAME='CUNI-MH'
 MODEL_NAME='/storage/brno12-cerit/home/hrabalm/workspace/wmt24_final_model'
+SCRIPT_DIR="$PBS_O_WORKDIR"
+cd "$SCRIPT_DIR" || exit
 
 source ./jobs/scripts/submit/fire/fire_S_smartt_metacentrum.sh
